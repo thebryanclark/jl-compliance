f3e4668dbb26db4d272cafd819e1e32a12e289f8
Bryan Clark <thebryanclark@gmail.com>
2012-12-17 23:53:47 -0500

    Commit some rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JlCompliance::Application.config.secret_token = '329a8596a70debf5cc14462c6e35cc8400492ce0a71f52a9687fc9f98ea61e40bae52e0f95339e9cd0ec891727a7b8cf895df9c235a1249f9371f3e0b3054f10'
