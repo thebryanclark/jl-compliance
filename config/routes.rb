03a2fb871d9386bf1fad2966cf7ff451d6def53d
Bryan Clark <thebryanclark@gmail.com>
2013-01-13 20:41:11 -0500

    jobs controller, jobs index

--- config/routes.rb
+++ config/routes.rb
@@ -60,5 +60,7 @@ JlCompliance::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  root :to => "home#index"
+  root :to => "jobs#index"
+
+  resources :jobs
 end
