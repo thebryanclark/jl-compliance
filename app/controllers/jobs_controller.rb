04a8cbf4c467f201059bd1d0f0c117ed9b6feca4
Bryan Clark <thebryanclark@gmail.com>
2013-01-15 22:01:44 -0500

    minor

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,12 @@ class JobsController < ApplicationController
 
   def index
     @jobs = Job.all
-    @title = "Jobs"  
+    @title = "Jobs"
+  end
+
+  def show
+    @job = Job.find(params[:id])
+    @title = @job.name
   end
 
 end
