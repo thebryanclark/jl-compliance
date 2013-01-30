59522713f6b99593e0f4fcd2b4625e9b1150beb2
Bryan Clark <thebryanclark@gmail.com>
2013-01-29 22:10:33 -0500

    Include FontAwesome and helper methods for easy use in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def icon(icon_name)
+    content_tag :span, ' ', class: "font-awesome icon-#{icon_name}"
+  end
+
+  def icon_if(boolean_value, true_icon, false_icon)
+    icon(boolean_value ? true_icon : false_icon)
+  end
+
+  def check_icon(boolean_value)
+    icon_if(boolean_value, 'check', 'check-empty')
+  end
 end
